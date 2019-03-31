b16b3dd721608ee508d47f277327faa41ee529de
Sunochi <cosmovs@yahoo.co.jp>
2019-03-31 17:45:40 +0900

    arrayの修正

--- src/php/index.php
+++ src/php/index.php
@@ -1,22 +1,31 @@
-<?php 
-$temp_array = [
-    'hoge' => "foo",
-    'var'  => 2,
-];
 
-function print_log(){
+n print_log(){
+    $temp_array = [
+        'hoge' => "foo",
+        'var'  => 2,
+    ];
     printf("Hello World!\hogehoge");
     printf($temp_array);
 }
 
 function var_dump_log(){
+    $temp_array = [
+        'hoge' => "foo",
+        'var'  => 2,
+    ];
     var_dump("Hello World!\hogehoge");
     var_dump($temp_array);
 }
 
 function error_log_log(){
+    $temp_array = [
+        'hoge' => "foo",
+        'var'  => 2,
+    ];
     error_log("Hello World!\nhogehoge");
     error_log($temp_array);
+    $json_temp_array = json_encode($temp_array);
+    error_log($json_temp_array);
 }
 
 function main(){
@@ -24,5 +33,3 @@ function main(){
     var_dump_log();
     error_log_log();
 }
-
-
